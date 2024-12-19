2a37517bab210d73a568cd7ffcaa4086623e337a
base60 <savory-dragnet-8h@icloud.com>
2024-12-19 03:39:39 -0800

    Retire chatboy for now

--- src/custom_node.py
+++
@@ -1,9 +0,0 @@
-from anytree import NodeMixin
-
-
-class CustomNode(NodeMixin):
-    def __init__(self, name, parent=None, node_type=None, relevance_score=1.0):
-        self.name = name
-        self.parent = parent
-        self.node_type = node_type
-        self.relevance_score = relevance_score
